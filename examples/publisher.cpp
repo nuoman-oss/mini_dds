ea6ab43667ff12c03a45c0a5747186d51663f6f0
nuoman-oss <3079103696@qq.com>
2026-09-13 22:49:33 +0800

    Initialize mini-DDS project

---
+++ examples/publisher.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    std::cout
+    <<"Mini DDS Publisher Start"
+    <<std::endl;
+    while(true)
+    {
+        std::cout<<"Publisher Data"<<std::endl;
+
+    }
+return 0;
+}
---
+++ examples/subscriber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main()
+{
+
+    std::cout
+        <<"Mini DDS Subscriber Start"
+        <<std::endl;
+
+
+    while(true)
+    {
+
+        std::cout
+        <<"Waiting Data..."
+        <<std::endl;
+
+    }
+
+
+    return 0;
+}
